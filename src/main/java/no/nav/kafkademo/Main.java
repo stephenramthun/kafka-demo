97228f999f73ebbb843c4202cc3627fb3639e64a
Stephen Ramthun <stephen.ramthun@bekk.no>
2018-10-15 15:02:30 +0200

    Refaktorer

--- src/main/java/no/nav/kafkademo/Main.java
+++ src/main/java/no/nav/kafkademo/Main.java
@@ -2,9 +2,7 @@ package no.nav.kafkademo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.kafka.annotation.EnableKafka;
 
-@EnableKafka
 @SpringBootApplication
 public class Main {
 
--- src/main/java/no/nav/kafkademo/config/KafkaConfig.java
+++ src/main/java/no/nav/kafkademo/config/KafkaConfig.java
@@ -3,10 +3,15 @@ package no.nav.kafkademo.config;
 import org.springframework.boot.autoconfigure.kafka.KafkaProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.kafka.annotation.EnableKafka;
+import org.springframework.kafka.config.ConcurrentKafkaListenerContainerFactory;
+import org.springframework.kafka.config.KafkaListenerContainerFactory;
 import org.springframework.kafka.core.*;
+import org.springframework.kafka.listener.ConcurrentMessageListenerContainer;
 
+@EnableKafka
 @Configuration
-public class KafkaProducerConfig {
+public class KafkaConfig {
 
     @Bean
     public ConsumerFactory<String, String> consumerFactory(KafkaProperties properties) {
@@ -21,6 +26,15 @@ public class KafkaProducerConfig {
     @Bean
     public KafkaTemplate<String, String> kafkaTemplate(ProducerFactory<String, String> producerFactory) {
         return new KafkaTemplate<>(producerFactory);
+    }
+
+    @Bean
+    public KafkaListenerContainerFactory<ConcurrentMessageListenerContainer<String, String>> kafkaListenerContainerFactory(ConsumerFactory<String, String> consumerFactory) {
+        ConcurrentKafkaListenerContainerFactory<String, String> factory;
+        factory = new ConcurrentKafkaListenerContainerFactory<>();
+        factory.setConsumerFactory(consumerFactory);
+
+        return factory;
     }
 
 }
